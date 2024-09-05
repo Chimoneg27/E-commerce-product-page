6a2dae6cd07c4663f0356f2746e25817b0fe6ee3
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-05 18:19:42 +0200

    finish slider JS code

--- JS/slider.js
+++ JS/slider.js
@@ -3,20 +3,36 @@ let slideIndex = 0;
 let intervalId = null;
 
 // initializeSlider()
-document.addEventListener("DOMContentLoaded", initializeSlider);
 
 const initializeSlider = () => {
+    if(slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000)
+    }
+}
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+const showSlide = (index) => {
+    if(index >= slides.length) {
+        slideIndex = 0; 
+    } else if(index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide")
+    })
+
     slides[slideIndex].classList.add("displaySlide")
 }
 
-const showSlide = (index) => {
-
-}
-
 const prevSlide = () => {
-
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
 }
 
 const nextSlide = () => {
-
+    slideIndex++;
+    showSlide(slideIndex);
 }
