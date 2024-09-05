0a937e8f15cf31094e8a92e5a095403532b66d3a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-05 17:55:14 +0200

    link slider js

---
+++ JS/slider.js
@@ -0,0 +1,22 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+// initializeSlider()
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+const initializeSlider = () => {
+    slides[slideIndex].classList.add("displaySlide")
+}
+
+const showSlide = (index) => {
+
+}
+
+const prevSlide = () => {
+
+}
+
+const nextSlide = () => {
+
+}
