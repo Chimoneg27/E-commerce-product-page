436e983f15f1ffe2355b2bd73cc9c54dc5593659
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-07 20:23:57 +0200

    add code for counting items

--- JS/cart.js
+++ JS/cart.js
@@ -2,13 +2,20 @@ let counter = 0;
 let cart = [];
 const minus = document.getElementById('minus');
 const plus = document.getElementById('plus');
+const cartCount = document.getElementById('cartCount');
 
 minus.addEventListener('click', () => {
   counter--;
+
+  if(counter === -1) {
+    counter = 0;
+  }
   document.getElementById('noItems').innerHTML = counter;
+  cartCount.innerHTML = counter;
 });
 
 plus.addEventListener('click', () => {
     counter++;
     document.getElementById('noItems').innerHTML = counter;
+    cartCount.innerHTML = counter;
 })
