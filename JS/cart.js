4a74823bf3fd5692b32a987da72e3be82bd576b2
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-09 17:46:16 +0200

    add logic for modal adn cart

--- JS/cart.js
+++ JS/cart.js
@@ -1,37 +1,53 @@
-let counter = 0;
-let cart = [];
-const minus = document.getElementById('minus');
-const plus = document.getElementById('plus');
-const cartCount = document.getElementById('cartCount');
-const addBtn = document.getElementById('addBtn');
-const showCart = document.getElementById('cartImg');
-const modal = document.getElementById('cartModal');
+let counter = 0
+let cart = []
+const minus = document.getElementById("minus")
+const plus = document.getElementById("plus")
+const cartCount = document.getElementById("cartCount")
+const addBtn = document.getElementById("addBtn")
+const showCart = document.getElementById("cartImg")
+const modal = document.getElementById("cartModal")
+const purchased = document.getElementById("purchased")
+const empty = document.getElementById("empty")
+const chckbtn = document.getElementById("chckbtn")
+const itemParagraph = document.querySelector('#item p');
 
-const addToCart = (item) => {
-    cart.push(item);
-}
+const addToCart = () => {
+  minus.addEventListener("click", () => {
+    counter--
 
-minus.addEventListener('click', () => {
-  counter--;
+    if (counter === -1) {
+      counter = 0
+    }
+    document.getElementById("noItems").innerHTML = counter
+    cartCount.innerHTML = counter
+  })
 
-  if(counter === -1) {
-    counter = 0;
+  plus.addEventListener("click", () => {
+    counter++
+    document.getElementById("noItems").innerHTML = counter
+    cartCount.innerHTML = counter
+  })
+
+  addBtn.addEventListener("click", () => {
+    cart.push(counter)
+    if(cart.length > 1) {
+      cart.shift()
+    }
+
+    if(counter > 0) {
+      cartCount.style.display = "flex"
+    }
+
+    if(cart.length > 0) {
+      empty.style.display = "none"
+      purchased.style.display = "flex"
+      chckbtn.style.display = "block"
+    }
+    console.log(cart)
+    itemParagraph.textContent = `${cart[0]} x $125.00`;})
   }
-  document.getElementById('noItems').innerHTML = counter;
-  cartCount.innerHTML = counter;
-});
 
-plus.addEventListener('click', () => {
-    counter++;
-    document.getElementById('noItems').innerHTML = counter;
-    
-    addBtn.addEventListener('click', () => {
-        addToCart(counter);
-    })
-    console.log(cart);
-    cartCount.innerHTML = counter;
-})
-
-showCart.addEventListener('click', () => {
-    modal.style.display = 'flex';
-})
+  showCart.addEventListener("click", () => {
+    modal.style.display = "flex"
+  })
+addToCart()
